967f4cb268ffc136458418efc380c4937c76f96c
Adrian Schmidt <adrian@stealmycode.se>
2014-03-14 18:57:46 +0100

    Adds Yeoman scaffolding for AngularJS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('weatherAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
