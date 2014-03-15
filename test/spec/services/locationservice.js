40829cebd47bb85db3fbfd8867b2c186ae7c3601
Adrian Schmidt <adrian@stealmycode.se>
2014-03-15 11:17:38 +0100

    Rename.

--- test/spec/services/locationservice.js
+++ test/spec/services/locationservice.js
@@ -1,18 +1,18 @@
 'use strict';
 
-describe('Service: Locationservice', function () {
+describe('Service: locationService', function () {
 
   // load the service's module
   beforeEach(module('weatherAppApp'));
 
   // instantiate service
-  var Locationservice;
+  var locationService;
   beforeEach(inject(function (_Locationservice_) {
-    Locationservice = _Locationservice_;
+    locationService = _Locationservice_;
   }));
 
   it('should do something', function () {
-    expect(!!Locationservice).toBe(true);
+    expect(!!locationService).toBe(true);
   });
 
 });
